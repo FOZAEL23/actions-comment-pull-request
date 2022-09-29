4828ffe01e510f91aac1e6a35391468e843113fc
FOZAEL <110026345+FOZAEL23@users.noreply.github.com>
2022-09-29 11:56:43 +0430

    some changes

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,16 @@
-const fs = require('fs');
-const path = require('path');
 import * as github from '@actions/github';
 import * as core from '@actions/core';
 import { GetResponseDataTypeFromEndpointMethod } from '@octokit/types';
 
-// See https://docs.github.com/en/rest/reactions#reaction-types
-const REACTIONS = ['+1', '-1', 'laugh', 'confused', 'heart', 'hooray', 'rocket', 'eyes'] as const;
-type Reaction = typeof REACTIONS[number];
-
 async function run() {
   try {
     let message: string = core.getInput('message');
-
     const github_token: string = core.getInput('GITHUB_TOKEN');
     const pr_number: string = core.getInput('pr_number');
-    const comment_includes: string = core.getInput('comment_includes');
 
     if (!message) {
       throw new Error('message input should be provided!');
     }
-
 
     const context = github.context;
     const pull_number = parseInt(pr_number) || context.payload.pull_request?.number;
@@ -31,45 +22,33 @@ async function run() {
       return;
     }
 
+    // type ListCommentsResponseDataType = GetResponseDataTypeFromEndpointMethod<
+    //   typeof octokit.rest.issues.listComments
+    // >;
+    // let comment: ListCommentsResponseDataType[0] | undefined;
+    // for await (const { data: comments } of octokit.paginate.iterator(octokit.rest.issues.listComments, {
+    //   ...context.repo,
+    //   issue_number: pull_number,
+    // })) {
+    //   comment = comments.find((comment) => comment?.body?.includes(message));
+    //   await octokit.rest.issues.updateComment({
+    //     ...context.repo,
+    //     comment_id: comment,
+    //     body: message,
+    //   });
+    // }
 
-    if (comment_includes) {
-      type ListCommentsResponseDataType = GetResponseDataTypeFromEndpointMethod<
-        typeof octokit.rest.issues.listComments
-      >;
-      let comment: ListCommentsResponseDataType[0] | undefined;
-      for await (const { data: comments } of octokit.paginate.iterator(octokit.rest.issues.listComments, {
-        ...context.repo,
-        issue_number: pull_number,
-      })) {
-        comment = comments.find((comment) => comment?.body?.includes(comment_includes));
-        if (comment) break;
-      }
-
-      if (comment) {
-        await octokit.rest.issues.updateComment({
-          ...context.repo,
-          comment_id: comment.id,
-          body: message,
-        });
-
-        return;
-      } else {
-        core.info('No comment has been found with asked pattern. Creating a new comment.');
-      }
-    }
-
-    const { data: comment } = await octokit.rest.issues.createComment({
+    //  get all comments from all pages
+    const { data: comments } = await octokit.rest.issues.listComments({
       ...context.repo,
       issue_number: pull_number,
-      body: message + '123456',
     });
 
-    const { data: hide } = await octokit.rest.issues.updateComment({
+    await octokit.rest.issues.updateComment({
       ...context.repo,
-      comment_id: comment.id,
+      comment_id: comments[0].id,
       body: message,
     });
-  
 
 
   } catch (error) {
